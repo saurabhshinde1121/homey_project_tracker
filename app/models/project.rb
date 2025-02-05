167bf7e41fa660e5d606d6795f40501c393f13fa
Saurabh Shinde <shindesaurabh824@gmail.com>
2025-02-05 12:49:12 +0530

    Fixed rubocop warnings

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
   has_many :project_histories, dependent: :destroy
   belongs_to :owner, class_name: 'User', optional: false
 
-  enum status: { todo: "Todo", in_progress: "In Progress", completed: "Completed", on_hold: "On Hold", cancelled: "Cancelled" }, _default: "todo"
+  enum status: {
+    todo: 'Todo',
+    in_progress: 'In Progress',
+    completed: 'Completed',
+    on_hold: 'On Hold',
+    cancelled: 'Cancelled'
+  }, _default: 'todo'
 end
--- db/migrate/20250205070250_devise_create_users.rb
+++ db/migrate/20250205070250_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
